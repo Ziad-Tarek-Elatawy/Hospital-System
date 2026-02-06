8ff97a1f2ebf56cbcf1017dd99f09ffd1f6e4318
yasmeenaladawy111 <yasmin.mansora.777@gmail.com>
2026-02-06 03:21:26 +0200

    Update SystemManager with Hospital type hinting

--- model/core/system_manager.py
+++ model/core/system_manager.py
@@ -1,18 +1,21 @@
+
+from model.hospital import Hospital
+
 class SystemManager:
-    """This class manages the hospital system display."""
-    def __init__(self, hospital):
-        self.hospital = hospital
+    def __init__(self, hospital: Hospital) -> None:
+        """Initializes the System Manager with a Hospital instance."""
+        self.hospital: Hospital = hospital
 
     def display_all_patients(self) -> None:
-        """Display all patients in the hospital."""
-        print(f"\n--- Patient List: {self.hospital.name} ---")
+        """Displays info for all patients in every department."""
+        print(f"\n--- Patient List for {self.hospital.name} ---")
         for dept in self.hospital.departments:
             for patient in dept.patients:
                 print(patient.view_info())
 
     def display_all_staff(self) -> None:
-        """Display all staff in the hospital."""
-        print(f"\n--- Staff List: {self.hospital.name} ---")
+        """Displays info for all staff members in every department."""
+        print(f"\n--- Staff List for {self.hospital.name} ---")
         for dept in self.hospital.departments:
             for member in dept.staff:
                 print(member.view_info())
